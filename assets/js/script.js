9e95f41d71f006a5cc649a85202892e964021ff9
Muhamad Fakhrusy <mfakhrusy@outlook.com>
2018-02-01 20:58:27 +0700

    finishing up site

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,13 +1,15 @@
 document.addEventListener("DOMContentLoaded", function(e) {
 
-  // modal function when clicked
+  // modal appear --> function when clicked
   const portfolioItems = document.getElementsByClassName("portfolio-item");
   const body = document.body;
+  const navbar = document.getElementById("page-nav");
 
   [].forEach.call(portfolioItems, function(portfolioItem, portfolioIndex) {
     portfolioItem.addEventListener("click", function(e) {
       // change bodyClass to modal-open for disable the scroll
       body.classList.toggle("modal-open");
+      navbar.classList.toggle("modal-open");
 
       // view the modal
       const portfolioItemId = portfolioItem.getAttribute('id');
@@ -21,11 +23,12 @@ document.addEventListener("DOMContentLoaded", function(e) {
     });
   });
 
-  // close the modal
+  // close the modal after clicking the close button
   const modalButtons = document.getElementsByClassName("modal-button");
   [].forEach.call(modalButtons, function(modalButton, modalButtonIndex) {
     modalButton.addEventListener("click", function(e) {
       body.classList.toggle("modal-open");
+      navbar.classList.toggle("modal-open");
       
       const portfolioItemId = modalButton.getAttribute('id');
       const re = /[1-9]+$/;
@@ -35,5 +38,49 @@ document.addEventListener("DOMContentLoaded", function(e) {
       modalItem.classList.toggle('modal-flex');
       modalItem.classList.toggle('modal-hidden');
     });
+  });
+
+  // change navbar color when scroll
+  const navbarText = document.querySelector("page-nav a");
+  document.addEventListener("scroll", function(e) {
+    if (window.pageYOffset > 10) {
+      navbar.classList.add("navbar-colored");
+    } else {
+      navbar.classList.remove("navbar-colored");
+    }
+  }); 
+
+  // smooth scrolling using jquery TODO change to vanilla JS for idk reason
+  $('a[href*="#"]')
+  // which not # and #0
+  .not('a[href="#"]')
+  .not('a[href="#0"]')
+  .not('a[href^=#portfolio-img]') // do not select the modal close button
+  .click(function(event) {
+
+    if (
+    location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') &&
+    location.hostname == this.hostname) {
+      let target = $(this.hash);
+      target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
+
+      if (target.length) { // target.length if true is = 1
+        event.preventDefault();
+        $('html, body').animate({
+          scrollTop: target.offset().top
+        }, 1000, "easeInOutCirc", function() { // easeInOutCirc only available in jquery UI
+          // callback after animation
+          // must change focus
+          let $target = $(target);
+          $target.focus();
+          if ($target.is(":focus")) {
+            return false;
+          } else {
+            $target.attr('tabindex','-1'); // Adding tabindex for elements not focusable
+            $target.focus(); // Set focus again
+          }
+        });
+      }
+    }
   });
 });
